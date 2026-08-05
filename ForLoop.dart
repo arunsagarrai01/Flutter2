9ec4ebebdc3b7c565fa79e21c1406b0998cfc237
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-08-05 13:29:48 +0545

    Learn about forloop

---
+++ ForLoop.dart
@@ -0,0 +1,16 @@
+/*void main() {
+  print("Hello World");
+  print("Hello World");
+  print("Hello World");
+  print("Hello World");
+  print("Hello World");
+}
+*/
+
+// Syntax of for loop in dart
+void main() {
+  for (int i = 1; i <= 100; i++) {
+    print("Hello World");
+  }
+}
+// Rendering a wedgit in flutter 100 times
