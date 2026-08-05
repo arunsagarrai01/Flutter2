9138ab40f5210db71d141896859ae52da58d4cb0
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-08-05 13:32:32 +0545

    Learn about Rendering a wedgit in flutter 100 times

--- ForLoop.dart
+++ ForLoop.dart
@@ -13,4 +13,36 @@ void main() {
     print("Hello World");
   }
 }
+
+
 // Rendering a wedgit in flutter 100 times
+
+/*
+
+// Rendering a wedgit in flutter 100 times
+
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+    children: [
+      for(int i = 1; i <= 100; i++)
+      Container(
+         margin: const EdgeInsets.all(10),
+        width: 100, height: 100, color: Colors.red)
+    ]
+    );
+  }
+}
+
+
+*/
