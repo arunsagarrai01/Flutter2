8ca83b3c6437e476bd42f24fc78f91409066409e
arunsagarrai01 <arunsagarrai6@gmail.com>
2026-08-12 20:43:33 +0545

    Learn about functions

---
+++ functions.dart
@@ -0,0 +1,17 @@
+void main() {
+  // functions
+  // perform a specific task that are coded inside a function
+  // functions are writen inside a class
+  // class inside we have functions
+  // how can you call the function
+  // task that need to be performed is a function
+  // main function is the entry point of the program
+  // void means you are not returning anything from the function
+  int result = (hundredthMultiple(2));
+  print(result);
+}
+
+// returnType functionname parameters
+int hundredthMultiple(int n) {
+  return n * 100;
+}
